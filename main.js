e9d24a7e9c362a83cda328fcf1748b8ef3fa73df
Serhat Zunluoğlu <97339604+serhatzunluoglu@users.noreply.github.com>
2023-06-22 20:59:58 +0300

    last updates

--- main.js
+++ main.js
@@ -1,17 +1,24 @@
 
-console.log("merhaba");
-
 let buton= document.querySelector(".navbar-bottom-categories");
+let bars= document.querySelector("#toggle_button_1");
+let xmark= document.querySelector("#toggle_button_2");
 
-console.log(buton.classList.contains("navbar-bottom-categories"));
 
 function categories_on(){
     if(buton.classList.contains("display-on")==true){
         buton.classList.remove("display-on");
         buton.classList.add("display-off");
+        xmark.classList.remove("display-on");
+        xmark.classList.add("display-off");
+        bars.classList.remove("display-off");
+        bars.classList.add("display-on");
     }
     else if(buton.classList.contains("display-off")==true){
         buton.classList.remove("display-off");
         buton.classList.add("display-on");
+        bars.classList.remove("display-on");
+        bars.classList.add("display-off");
+        xmark.classList.remove("display-off");
+        xmark.classList.add("display-on");
     }
 }
